cb19c23dc2d5617b436161404dcec7ab3206ce90
heiqie <heiqie@wacai.com>
2017-11-11 11:21:31 +0800

    add gitignore

---
+++ src/main/java/com/freeland/HorusApplication.java
@@ -0,0 +1,12 @@
+package com.freeland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HorusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HorusApplication.class, args);
+	}
+}
